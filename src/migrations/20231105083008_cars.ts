f7fbb678f65ad831ba94abf29d131046bda2d953
Misbahudin <misbahudin.id@gmail.com>
2023-11-11 11:43:26 +0700

    feat: uuid

--- src/migrations/20231105083008_cars.ts
+++ src/migrations/20231105083008_cars.ts
@@ -1,25 +1,25 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema.createTable('cars', (table: Knex.TableBuilder) => {
-        table.increments('id').primary();
-        table.string('plate').notNullable();
-        table.string('manufacture').notNullable();
-        table.string('model').notNullable();
-        table.string('image').notNullable();
-        table.integer('rentPerDay').notNullable();
-        table.integer('capacity').notNullable();
-        table.string('description').notNullable();
-        table.timestamp('availableAt').notNullable().defaultTo(knex.fn.now());
-        table.string('transmission').notNullable();
-        table.boolean('available').notNullable().defaultTo(false);
-        table.string('type').notNullable();
-        table.integer('year').notNullable();
-        table.json('options').notNullable();
-        table.json('specs').notNullable();
-    });
+  return knex.schema.createTable('cars', (table: Knex.TableBuilder) => {
+    table.uuid('id').defaultTo(knex.fn.uuid()).primary();
+    table.string('plate').notNullable();
+    table.string('manufacture').notNullable();
+    table.string('model').notNullable();
+    table.string('image').notNullable();
+    table.integer('rentPerDay').notNullable();
+    table.integer('capacity').notNullable();
+    table.string('description').notNullable();
+    table.timestamp('availableAt').notNullable().defaultTo(knex.fn.now());
+    table.string('transmission').notNullable();
+    table.boolean('available').notNullable().defaultTo(false);
+    table.string('type').notNullable();
+    table.integer('year').notNullable();
+    table.json('options').notNullable();
+    table.json('specs').notNullable();
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTableIfExists('cars');
+  return knex.schema.dropTableIfExists('cars');
 }
