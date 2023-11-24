4c7341b9a59dee7eaceccb48e0ed85ee2bc2352e
Misbahudin <misbahudin.id@gmail.com>
2023-11-24 18:58:24 +0700

    docs: swagger openapi

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -17,11 +17,355 @@ const usersController = new UsersController();
 appRouter.get('/', authToken, mainController.index);
 appRouter.get('/cars', authToken, mainController.searchCars);
 
+/**
+ * @openapi
+ * /api/v1/cars:
+ *  get:
+ *    summary: Get all car
+ *    description: Get all car
+ *    tags:
+ *      - Cars
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                 id:
+ *                  type: string
+ *                 plate:
+ *                  type: string
+ *                 manufacture:
+ *                  type: string
+ *                 model:
+ *                  type: string
+ *                 image:
+ *                  type: string
+ *                 rentPerDay:
+ *                  type: number
+ *                 capacity:
+ *                  type: number
+ *                 description:
+ *                  type: string
+ *                 availableAt:
+ *                  type: date
+ *                 transmission:
+ *                  type: string
+ *                 available:
+ *                  type: boolean
+ *                 type:
+ *                  type: string
+ *                 year:
+ *                  type: number
+ *                 options:
+ *                  type: array
+ *                  items:
+ *                    type: string
+ *                 specs:
+ *                  type: array
+ *                  items:
+ *                    type: string
+ *                 created_by:
+ *                  type: string
+ *                 updated_by:
+ *                  type: string
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Invalid Token
+ */
 apiRouter.get('/api/v1/cars', authToken, carsController.getCarAll);
+
+/**
+ * @openapi
+ * /api/v1/cars:
+ *  delete:
+ *    summary: Delete all car
+ *    description: Delete all car
+ *    tags:
+ *      - Cars
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *                 statusCode:
+ *                   type: number
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Invalid Token
+ */
 apiRouter.delete('/api/v1/cars', authToken, carsController.deleteCar);
+
+/**
+ * @openapi
+ * /api/v1/cars/{id}:
+ *  get:
+ *    summary: Get detail car
+ *    description: Get detail car
+ *    tags:
+ *      - Cars
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *          format: uuid
+ *        description: UUID of car
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *                type: object
+ *                properties:
+ *                 id:
+ *                  type: string
+ *                 plate:
+ *                  type: string
+ *                 manufacture:
+ *                  type: string
+ *                 model:
+ *                  type: string
+ *                 image:
+ *                  type: string
+ *                 rentPerDay:
+ *                  type: number
+ *                 capacity:
+ *                  type: number
+ *                 description:
+ *                  type: string
+ *                 availableAt:
+ *                  type: date
+ *                 transmission:
+ *                  type: string
+ *                 available:
+ *                  type: boolean
+ *                 type:
+ *                  type: string
+ *                 year:
+ *                  type: number
+ *                 options:
+ *                  type: array
+ *                  items:
+ *                    type: string
+ *                 specs:
+ *                  type: array
+ *                  items:
+ *                    type: string
+ *                 created_by:
+ *                  type: string
+ *                 updated_by:
+ *                  type: string
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Invalid Token
+ */
 apiRouter.get('/api/v1/cars/:id', authToken, carsController.getDetailCar);
+
+/**
+ * @openapi
+ * /api/v1/cars/{id}:
+ *  patch:
+ *    summary: Update car
+ *    description: Delete car by id
+ *    tags:
+ *      - Cars
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *          format: uuid
+ *        description: UUID of car
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/jsonBodyCar'
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *                 statusCode:
+ *                   type: number
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Invalid Token
+ */
 apiRouter.patch('/api/v1/cars/:id', authToken, carsController.patchEditCar);
+
+/**
+ * @openapi
+ * /api/v1/cars/{id}:
+ *  delete:
+ *    summary: Delete car
+ *    description: Delete car by id
+ *    tags:
+ *      - Cars
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *          format: uuid
+ *        description: UUID of car
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *                 statusCode:
+ *                   type: number
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Invalid Token
+ */
 apiRouter.delete('/api/v1/cars/:id', authToken, carsController.deleteCarById);
+
+/**
+ * @openapi
+ * /api/v1/cars:
+ *  post:
+ *    summary: Create a new car
+ *    description: Create a new car
+ *    tags:
+ *      - Cars
+ *    security:
+ *      - bearerAuth: []
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/jsonBodyCar'
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                 id:
+ *                  type: string
+ *                 plate:
+ *                  type: string
+ *                 manufacture:
+ *                  type: string
+ *                 model:
+ *                  type: string
+ *                 image:
+ *                  type: string
+ *                 rentPerDay:
+ *                  type: number
+ *                 capacity:
+ *                  type: number
+ *                 description:
+ *                  type: string
+ *                 availableAt:
+ *                  type: date
+ *                 transmission:
+ *                  type: string
+ *                 available:
+ *                  type: boolean
+ *                 type:
+ *                  type: string
+ *                 year:
+ *                  type: number
+ *                 options:
+ *                  type: array
+ *                 specs:
+ *                  type: array
+ *                 created_by:
+ *                  type: string
+ *                 updated_by:
+ *                  type: string
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Invalid Token
+ */
 apiRouter.post('/api/v1/cars', authToken, upload.single('image'), carsController.postSaveCar);
 
 /**
@@ -142,6 +486,8 @@ apiRouter.post('/api/v1/users/register', usersController.register);
  *    description: Add a new user
  *    tags:
  *      - Users
+ *    security:
+ *      - bearerAuth: []
  *    requestBody:
  *      content:
  *        application/json:
@@ -224,11 +570,13 @@ apiRouter.post('/api/v1/users/add', authToken, usersController.addUser);
 /**
  * @openapi
  * /api/v1/users/profile:
- *  post:
+ *  get:
  *    summary: Get current user
  *    description: Get profile user
  *    tags:
  *      - Users
+ *    security:
+ *      - bearerAuth: []
  *    responses:
  *      200:
  *        description: OK
@@ -262,6 +610,49 @@ apiRouter.post('/api/v1/users/add', authToken, usersController.addUser);
  *                  example: Invalid token!
  */
 apiRouter.get('/api/v1/users/profile', authToken, usersController.profile);
+
+/**
+ * @openapi
+ * components:
+ *      schemas:
+ *          jsonBodyCar:
+ *              type: object
+ *              properties:
+ *                  plate:
+ *                      type: string
+ *                  manufacture:
+ *                      type: string
+ *                  model:
+ *                      type: string
+ *                  image:
+ *                      type: string
+ *                  rentPerDay:
+ *                      type: integer
+ *                  capacity:
+ *                      type: integer
+ *                      minimum: 1
+ *                  description:
+ *                      type: string
+ *                  availableAt:
+ *                      type: string
+ *                      format: date-time
+ *                  transmission:
+ *                      type: string
+ *                  available:
+ *                      type: boolean
+ *                  type:
+ *                      type: string
+ *                  year:
+ *                      type: integer
+ *                  options:
+ *                      type: array
+ *                      items:
+ *                          type: string
+ *                  specs:
+ *                      type: array
+ *                      items:
+ *                          type: string
+ */
 
 apiRouter.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
