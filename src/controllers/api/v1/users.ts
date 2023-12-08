f2f6b185ad7cf38eb8a6d04deff61d3a47300012
Misbahudin <misbahudin.id@gmail.com>
2023-12-08 19:09:18 +0700

    prettier

--- src/controllers/api/v1/users.ts
+++ src/controllers/api/v1/users.ts
@@ -1,95 +1,95 @@
-import { Request, Response } from 'express';
-import { Users } from '../../../models/users';
-import { UserService } from '../../../services/UserService';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { readFileSync } from 'fs';
-import path from 'path';
+import { Request, Response } from 'express'
+import { Users } from '../../../models/users'
+import { UserService } from '../../../services/UserService'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { readFileSync } from 'fs'
+import path from 'path'
 
 export class UsersController {
-  userService: UserService;
-  privateKey: Buffer;
+    userService: UserService
+    privateKey: Buffer
 
-  constructor() {
-    this.userService = new UserService();
-    this.privateKey = readFileSync(path.join(process.cwd(), 'keys', 'jwtRS256.key'));
-  }
-
-  register = async (req: Request<{}, {}, Users>, res: Response) => {
-    try {
-      const body = {
-        email: req.body.email,
-        name: req.body.name,
-        password: await bcrypt.hash(req.body.password, 10),
-      };
-
-      const user = await this.userService.saveUser(body);
-
-      return res.status(201).json({
-        user: {
-          email: user.email,
-          name: user.name,
-          role: user.role,
-        },
-      });
-    } catch (err: any) {
-      console.error(err);
-      return res.status(409).json({ message: 'Email already exist' });
+    constructor() {
+        this.userService = new UserService()
+        this.privateKey = readFileSync(path.join(process.cwd(), 'keys', 'jwtRS256.key'))
     }
-  };
 
-  login = async (req: Request, res: Response) => {
-    try {
-      const { email, password } = req.body;
-      const user = await this.userService.getUserByEmail(email);
+    register = async (req: Request<{}, {}, Users>, res: Response) => {
+        try {
+            const body = {
+                email: req.body.email,
+                name: req.body.name,
+                password: await bcrypt.hash(req.body.password, 10),
+            }
 
-      if (!(await bcrypt.compare(password, user[0].password))) {
-        return res.status(400).json({ message: 'Email or Password is wrong' });
-      }
-      // token will expire in one hour
-      const token = jwt.sign(
-        { id: user[0].id, email: user[0].email, name: user[0].name, role: user[0].role },
-        this.privateKey,
-        {
-          expiresIn: '1h',
-          algorithm: 'RS256',
+            const user = await this.userService.saveUser(body)
+
+            return res.status(201).json({
+                user: {
+                    email: user.email,
+                    name: user.name,
+                    role: user.role,
+                },
+            })
+        } catch (err: any) {
+            console.error(err)
+            return res.status(409).json({ message: 'Email already exist' })
         }
-      );
-
-      return res.status(200).json({ token });
-    } catch (err: any) {
-      console.error(err);
-      err.name = 'Email or Password is wrong';
-      return res.status(400).json({ message: err });
     }
-  };
 
-  profile = async (req: Request, res: Response) => {
-    return res.status(200).json(req.user);
-  };
+    login = async (req: Request, res: Response) => {
+        try {
+            const { email, password } = req.body
+            const user = await this.userService.getUserByEmail(email)
 
-  addUser = async (req: Request<{}, {}, Users>, res: Response) => {
-    try {
-      if (req.user.role !== 'superadmin' && req.user.role !== 'admin') {
-        return res.status(401).json({ message: 'Only role superadmin or admin!' });
-      }
+            if (!(await bcrypt.compare(password, user[0].password))) {
+                return res.status(400).json({ message: 'Email or Password is wrong' })
+            }
+            // token will expire in one hour
+            const token = jwt.sign(
+                { id: user[0].id, email: user[0].email, name: user[0].name, role: user[0].role },
+                this.privateKey,
+                {
+                    expiresIn: '1h',
+                    algorithm: 'RS256',
+                }
+            )
 
-      const body = {
-        email: req.body.email,
-        name: req.body.name,
-        password: await bcrypt.hash(req.body.password, 10),
-        role: req.body.role,
-      };
-
-      const user = await this.userService.saveUser(body);
-
-      return res.status(201).json({ user });
-    } catch (err: any) {
-      console.error(err);
-      if (err.constraint === 'users_role_check') {
-        return res.status(400).json({ message: 'The value of role is not allowed' });
-      }
-      return res.status(409).json({ message: 'Email already exist' });
+            return res.status(200).json({ token })
+        } catch (err: any) {
+            console.error(err)
+            err.name = 'Email or Password is wrong'
+            return res.status(400).json({ message: err })
+        }
     }
-  };
+
+    profile = async (req: Request, res: Response) => {
+        return res.status(200).json(req.user)
+    }
+
+    addUser = async (req: Request<{}, {}, Users>, res: Response) => {
+        try {
+            if (req.user.role !== 'superadmin' && req.user.role !== 'admin') {
+                return res.status(401).json({ message: 'Only role superadmin or admin!' })
+            }
+
+            const body = {
+                email: req.body.email,
+                name: req.body.name,
+                password: await bcrypt.hash(req.body.password, 10),
+                role: req.body.role,
+            }
+
+            const user = await this.userService.saveUser(body)
+
+            return res.status(201).json({ user })
+        } catch (err: any) {
+            console.error(err)
+            if (err.constraint === 'users_role_check') {
+                return res.status(400).json({ message: 'The value of role is not allowed' })
+            }
+            return res.status(409).json({ message: 'Email already exist' })
+        }
+    }
 }
--- src/repository/CarRepository.ts
+++ src/repository/CarRepository.ts
@@ -1,31 +1,31 @@
-import { Cars, CarsModel } from '../models/cars';
+import { Cars, CarsModel } from '../models/cars'
 
 export class CarRepository {
-  save = async (car: Cars) => {
-    return await CarsModel.query().insert(car).returning('id');
-  };
+    save = async (car: Cars) => {
+        return await CarsModel.query().insert(car).returning('id')
+    }
 
-  findAll = async () => {
-    return await CarsModel.query();
-  };
+    findAll = async () => {
+        return await CarsModel.query()
+    }
 
-  findById = async (id: string) => {
-    return await CarsModel.query().findById(id);
-  };
+    findById = async (id: string) => {
+        return await CarsModel.query().findById(id)
+    }
 
-  findByAvailableAt = async (rentalDate: Date) => {
-    return await CarsModel.query().where('availableAt', '>', rentalDate).orderBy('available_at');
-  };
+    findByAvailableAt = async (rentalDate: Date) => {
+        return await CarsModel.query().where('availableAt', '>', rentalDate).orderBy('available_at')
+    }
 
-  updateById = async (id: string, car: Partial<Cars>) => {
-    return await CarsModel.query().patch(car).where({ id }).throwIfNotFound().returning('*');
-  };
+    updateById = async (id: string, car: Partial<Cars>) => {
+        return await CarsModel.query().patch(car).where({ id }).throwIfNotFound().returning('*')
+    }
 
-  deleteById = async (id: string) => {
-    return await CarsModel.query().deleteById(id);
-  };
+    deleteById = async (id: string) => {
+        return await CarsModel.query().deleteById(id)
+    }
 
-  deleteAll = async () => {
-    return await CarsModel.query().delete();
-  };
+    deleteAll = async () => {
+        return await CarsModel.query().delete()
+    }
 }
