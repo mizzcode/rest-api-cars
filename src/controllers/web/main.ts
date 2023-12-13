ab04280217d620002b7b6ff5913f7b9bf7c9d7fb
Misbahudin <misbahudin.id@gmail.com>
2023-12-13 20:05:37 +0700

    prettier tab

--- src/controllers/web/main.ts
+++ src/controllers/web/main.ts
@@ -1,42 +1,42 @@
-import { Request, Response } from 'express';
-import { CarService } from '../../services/CarService';
+import { Request, Response } from 'express'
+import { CarService } from '../../services/CarService'
 
 interface IReqQuery {
-  driver: number;
-  date: string;
-  pickupTime: string;
-  totalPassenger: number;
+    driver: number
+    date: string
+    pickupTime: string
+    totalPassenger: number
 }
 
 export class MainController {
-  carService: CarService;
+    carService: CarService
 
-  constructor() {
-    this.carService = new CarService();
-  }
-
-  index(_: Request, res: Response) {
-    return res.render('index');
-  }
-
-  searchCars = async (req: Request<{}, {}, {}, IReqQuery>, res: Response) => {
-    const { driver, date, pickupTime, totalPassenger } = req.query;
-
-    let availableCars = await this.carService.getCarAll();
-
-    if (date && pickupTime) {
-      const rentalDate = new Date(date + pickupTime);
-
-      availableCars = await this.carService.getCarByAvailableAt(rentalDate);
+    constructor() {
+        this.carService = new CarService()
     }
 
-    // mencari mobil dengan kapasitas terbesar
-    const maxCarCapacity = Math.max(...availableCars.map((car: any) => car.capacity));
-    // format rupiah
-    const rupiah = this.carService.rupiah;
+    index(_: Request, res: Response) {
+        return res.render('index')
+    }
 
-    let foundCar = true;
+    searchCars = async (req: Request<{}, {}, {}, IReqQuery>, res: Response) => {
+        const { driver, date, pickupTime, totalPassenger } = req.query
 
-    return res.render('cars', { availableCars, driver, totalPassenger, maxCarCapacity, foundCar, rupiah });
-  };
+        let availableCars = await this.carService.getCarAll()
+
+        if (date && pickupTime) {
+            const rentalDate = new Date(date + pickupTime)
+
+            availableCars = await this.carService.getCarByAvailableAt(rentalDate)
+        }
+
+        // mencari mobil dengan kapasitas terbesar
+        const maxCarCapacity = Math.max(...availableCars.map((car: any) => car.capacity))
+        // format rupiah
+        const rupiah = this.carService.rupiah
+
+        let foundCar = true
+
+        return res.render('cars', { availableCars, driver, totalPassenger, maxCarCapacity, foundCar, rupiah })
+    }
 }
