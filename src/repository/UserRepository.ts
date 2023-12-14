de05284a1800d34af79f952df951deb0ba8ed11d
Misbahudin <misbahudin.id@gmail.com>
2023-12-15 00:17:28 +0700

    feat: add return type

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,15 +1,16 @@
-import { UsersModel, Users } from '../models/users'
+import { UsersModel } from '../models/users'
+import type { Users } from '../models/users'
 
 export class UserRepository {
-    save = async (user: Partial<Users>) => {
+    save = async (user: Partial<Users>): Promise<UsersModel> => {
         return await UsersModel.query().insert(user).returning(['email', 'name', 'role'])
     }
 
-    findUserByEmail = async (email: string) => {
+    findUserByEmail = async (email: string): Promise<UsersModel[]> => {
         return await UsersModel.query().where('email', email).throwIfNotFound()
     }
 
-    deleteAllUser = async () => {
+    deleteAllUser = async (): Promise<number> => {
         return await UsersModel.query().delete()
     }
 }
