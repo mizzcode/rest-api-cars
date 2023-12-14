eedd3e0ac503f77d598751fc83a799009b8af44d
Misbahudin <misbahudin.id@gmail.com>
2023-12-14 23:43:06 +0700

    feat: add return type

--- src/utils/routeNotFound.ts
+++ src/utils/routeNotFound.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express'
 
-export const routeNotFound = async (req: Request, res: Response) => {
-  // jika ada request dengan endpoint selain yang di daftarkan di app express nya, kirim response endpoint belum di daftarkan
-  res.status(404).json({
-    status: 'Fail',
-    message: `Endpoint ${req.originalUrl} is not registered on the server!`,
-    statusCode: 404,
-  });
-};
+export const routeNotFound = (req: Request, res: Response): void => {
+    // jika ada request dengan endpoint selain yang di daftarkan di app express nya, kirim response endpoint belum di daftarkan
+    res.status(404).json({
+        status: 'Fail',
+        message: `Endpoint ${req.originalUrl} is not registered on the server!`,
+        statusCode: 404,
+    })
+}
