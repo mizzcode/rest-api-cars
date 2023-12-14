c4b5007a74610d0e03b4315dda8f17fecd9f7259
Misbahudin <misbahudin.id@gmail.com>
2023-12-15 00:08:41 +0700

    feat: add  type and return type

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,27 +1,26 @@
-import { Model, ModelObject } from 'objection';
+import { Model } from 'objection'
+import type { JSONSchema, ModelObject } from 'objection'
 
 export class UsersModel extends Model {
-  // membuat type Users
-  id!: number;
-  email!: string;
-  name!: string;
-  password!: string;
-  role!: string;
+    // membuat type Users
+    id!: number
+    email!: string
+    name!: string
+    password!: string
+    role!: string
 
-  static get tableName() {
-    return 'users';
-  }
-  // validasi http request body
-  static get jsonSchema() {
-    return {
-      type: 'object',
-      required: ['email', 'name', 'password'],
-      properties: {
-        name: { type: 'string', minLength: 3 },
-        password: { type: 'string', minLength: 6 },
-      },
-    };
-  }
+    static readonly tableName = 'users'
+    // validasi http request body
+    static get jsonSchema(): JSONSchema {
+        return {
+            type: 'object',
+            required: ['email', 'name', 'password'],
+            properties: {
+                name: { type: 'string', minLength: 3 },
+                password: { type: 'string', minLength: 6 },
+            },
+        }
+    }
 }
 
-export type Users = ModelObject<UsersModel>;
+export type Users = ModelObject<UsersModel>
