0fdd8aa69e598740b73a59e6eef399930f0299f2
Misbahudin <misbahudin.id@gmail.com>
2023-12-17 10:00:57 +0700

    feat: test port but in coverage, it does  not effect

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,7 +1,11 @@
 import { describe, it, expect } from 'vitest'
-import { server } from '../index'
+import { port, server } from '../index'
 
 describe('test server', () => {
+    it('should be using port for http server', () => {
+        expect(port).toBe('3000')
+    })
+
     it('server should be running', () => {
         expect(server.listening).toBe(true)
 
