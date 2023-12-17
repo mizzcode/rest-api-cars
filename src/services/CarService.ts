453b393aa30d96b4a47f5a95588f2caecbf24fa3
Misbahudin <misbahudin.id@gmail.com>
2023-12-17 09:58:26 +0700

    feat: return type

--- src/services/CarService.ts
+++ src/services/CarService.ts
@@ -1,45 +1,45 @@
-import { Cars } from '../models/cars';
-import { CarRepository } from '../repository/CarRepository';
+import type { Cars, CarsModel } from '../models/cars'
+import { CarRepository } from '../repository/CarRepository'
 export class CarService {
-  carRepository: CarRepository;
-  constructor() {
-    this.carRepository = new CarRepository();
-  }
+    carRepository: CarRepository
+    constructor() {
+        this.carRepository = new CarRepository()
+    }
 
-  rupiah = (number: number) => {
-    return new Intl.NumberFormat('id-ID', {
-      style: 'currency',
-      currency: 'IDR',
-      maximumFractionDigits: 0,
-      minimumFractionDigits: 0,
-    }).format(number);
-  };
+    rupiah = (number: number): string => {
+        return new Intl.NumberFormat('id-ID', {
+            style: 'currency',
+            currency: 'IDR',
+            maximumFractionDigits: 0,
+            minimumFractionDigits: 0,
+        }).format(number)
+    }
 
-  addCar = async (car: Cars) => {
-    return await this.carRepository.save(car);
-  };
+    addCar = async (car: Cars): Promise<CarsModel> => {
+        return await this.carRepository.save(car)
+    }
 
-  getCarAll = async () => {
-    return await this.carRepository.findAll();
-  };
+    getCarAll = async (): Promise<CarsModel[]> => {
+        return await this.carRepository.findAll()
+    }
 
-  getCarById = async (id: string) => {
-    return await this.carRepository.findById(id);
-  };
+    getCarById = async (id: string): Promise<CarsModel | undefined> => {
+        return await this.carRepository.findById(id)
+    }
 
-  getCarByAvailableAt = async (rentalDate: Date) => {
-    return await this.carRepository.findByAvailableAt(rentalDate);
-  };
+    getCarByAvailableAt = async (rentalDate: Date): Promise<CarsModel[]> => {
+        return await this.carRepository.findByAvailableAt(rentalDate)
+    }
 
-  editCar = async (id: string, car: Partial<Cars>) => {
-    return await this.carRepository.updateById(id, car);
-  };
+    editCar = async (id: string, car: Partial<Cars>): Promise<CarsModel[]> => {
+        return await this.carRepository.updateById(id, car)
+    }
 
-  deleteCarById = async (id: string) => {
-    return await this.carRepository.deleteById(id);
-  };
+    deleteCarById = async (id: string): Promise<number> => {
+        return await this.carRepository.deleteById(id)
+    }
 
-  deleteCarAll = async () => {
-    return await this.carRepository.deleteAll();
-  };
+    deleteCarAll = async (): Promise<number> => {
+        return await this.carRepository.deleteAll()
+    }
 }
