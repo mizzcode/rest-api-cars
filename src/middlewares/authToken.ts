a4cfaa3b1987bdf7ae03aa8a2abfad5471edc093
Misbahudin <misbahudin.id@gmail.com>
2023-12-19 06:58:37 +0700

    feat: add return type

--- src/middlewares/authToken.ts
+++ src/middlewares/authToken.ts
@@ -1,9 +1,13 @@
-import { Request, Response, NextFunction } from 'express'
+import type { Request, Response, NextFunction } from 'express'
 import { readFileSync } from 'fs'
 import jwt from 'jsonwebtoken'
 import path from 'path'
 
-export const authToken = async (req: Request<{}, {}, {}, {}>, res: Response, next: NextFunction) => {
+export const authToken = async (
+    req: Request<unknown, unknown, unknown, unknown>,
+    res: Response,
+    next: NextFunction
+): Promise<Response<any, Record<string, any>> | undefined> => {
     try {
         const bearerToken = req.headers.authorization
         // Leave the Bearer
@@ -11,17 +15,17 @@ export const authToken = async (req: Request<{}, {}, {}, {}>, res: Response, nex
 
         const privateKey = readFileSync(path.join(process.cwd(), 'keys', 'jwtRS256.key'))
 
-        if (!token) {
+        if (token === undefined) {
             return res.status(401).json({ message: 'Invalid Token' })
         }
 
         jwt.verify(token, privateKey, (err, user) => {
-            if (err) {
+            if (err === null) {
                 return res.status(403).json({ message: 'Forbidden' })
             }
-            // @ts-expect-error
+            // @ts-expect-error user
             req.user = user
-            return next()
+            next()
         })
     } catch (err: any) {
         console.error(err)
