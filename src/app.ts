25e627ba0b65604dee77849196d8a12b011d7fe6
Misbahudin <misbahudin.id@gmail.com>
2023-12-21 19:02:54 +0700

    fix cors dev

--- src/app.ts
+++ src/app.ts
@@ -31,6 +31,7 @@ class App {
             cors({
                 origin: [
                     'http://127.0.0.1:5173',
+                    'http://127.0.0.1:3000',
                     'https://rest-api-cars.fly.dev/',
                     'https://react-rentalcar.netlify.app',
                 ],
