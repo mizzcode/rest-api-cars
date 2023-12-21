261d46a59b7244230a6fb403eebda5c667533af9
Misbahudin <misbahudin.id@gmail.com>
2023-12-21 18:24:44 +0700

    fix listen

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import App from './app'
 
-export const port = Number(process.env.PORT) ?? 3000
+export const port = Number(process.env.PORT) ?? 4000
 
 export const server = new App().startServer(port)
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,14 +1,8 @@
 import { describe, it, expect } from 'vitest'
-import { port, server } from '../index'
+import { port } from '../index'
 
 describe('test server', () => {
     it('should be using port for http server', () => {
         expect(port).toBeTruthy()
-    })
-
-    it('server should be running', () => {
-        expect(server.listening).toBe(true)
-
-        server.close()
     })
 })
