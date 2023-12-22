7eb142dde26700dfc3391ae864c59e657447abeb
Misbahudin <misbahudin.id@gmail.com>
2023-12-22 20:26:09 +0700

    feat: add default value param

--- src/utils/createUser.ts
+++ src/utils/createUser.ts
@@ -3,12 +3,16 @@ import bcrypt from 'bcrypt'
 
 const userRepository = new UserRepository()
 
-export const createUser = async (email: string = 'mizz@gmail.com', name: string = 'Misbah'): Promise<boolean> => {
+export const createUser = async (
+    email: string = 'mizz@gmail.com',
+    name: string = 'Misbah',
+    role: string = 'superadmin'
+): Promise<boolean> => {
     await userRepository.save({
         email,
         name,
         password: await bcrypt.hash('password', 10),
-        role: 'superadmin',
+        role,
     })
     return true
 }
