1e571e54a7dae302f513e8dc35aacb83e5e359e5
Misbahudin <misbahudin.id@gmail.com>
2023-12-23 03:36:09 +0700

    test: route

--- src/tests/route.test.ts
+++ src/tests/route.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest'
 import supertest from 'supertest'
 import App from '../app'
 
-describe('test middleware auth token', () => {
+describe('test route', () => {
     const app = new App().app
     it('should be route not found', async () => {
         const response = await supertest(app).get('/api/v1/notfound')
