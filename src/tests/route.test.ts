25274132e03b06613c36cfd226e78e0eaeeca0a8
Misbahudin <misbahudin.id@gmail.com>
2023-12-23 03:36:21 +0700

    test: route

--- src/tests/route.test.ts
+++ src/tests/route.test.ts
@@ -4,6 +4,7 @@ import App from '../app'
 
 describe('test route', () => {
     const app = new App().app
+
     it('should be route not found', async () => {
         const response = await supertest(app).get('/api/v1/notfound')
 
