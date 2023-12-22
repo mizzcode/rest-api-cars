b4a36cfcf36b07b7a5c03f34fe12d7aa28c311e8
Misbahudin <misbahudin.id@gmail.com>
2023-12-23 03:28:44 +0700

    test: route not found

---
+++ src/tests/route.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from 'vitest'
+import supertest from 'supertest'
+import App from '../app'
+
+describe('test middleware auth token', () => {
+    const app = new App().app
+    it('should be route not found', async () => {
+        const response = await supertest(app).get('/api/v1/notfound')
+
+        expect(response.statusCode).toBe(404)
+    })
+})
