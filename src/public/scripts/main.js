36e83707ad78022df72601c58d0d1b7fa2db1856
Misbahudin <misbahudin.id@gmail.com>
2023-12-24 19:23:16 +0700

    disable eslint in js

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -1,36 +1,37 @@
-class App {
-  constructor() {
-    // get element form search car
-    this.formSearchCar = document.getElementById('searchCar');
-    this.resultCar = document.getElementById('resultCar');
-  }
-
-  init() {
-    const typeDriver = document.getElementById('driver');
-    const date = document.getElementById('date');
-    const pickupTime = document.getElementById('pickupTime');
-    const totalPassenger = document.getElementById('totalPassenger');
-    const submitButton = document.getElementById('cariMobil');
-
-    typeDriver.addEventListener('change', enableSubmitButton);
-    date.addEventListener('input', enableSubmitButton);
-    pickupTime.addEventListener('change', enableSubmitButton);
-    totalPassenger.addEventListener('input', enableSubmitButton);
-
-    function enableSubmitButton() {
-      if (
-        typeDriver.value !== 'Pilih Tipe Driver' &&
-        date.value !== '' &&
-        pickupTime.value !== 'Pilih Waktu' &&
-        totalPassenger.value !== ''
-      ) {
-        submitButton.removeAttribute('disabled');
-      } else {
-        submitButton.setAttribute('disabled', true);
-      }
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export class App {
+    constructor() {
+        // get element form search car
+        this.formSearchCar = document.getElementById('searchCar')
+        this.resultCar = document.getElementById('resultCar')
     }
-  }
+
+    init() {
+        const typeDriver = document.getElementById('driver')
+        const date = document.getElementById('date')
+        const pickupTime = document.getElementById('pickupTime')
+        const totalPassenger = document.getElementById('totalPassenger')
+        const submitButton = document.getElementById('cariMobil')
+
+        typeDriver.addEventListener('change', enableSubmitButton)
+        date.addEventListener('input', enableSubmitButton)
+        pickupTime.addEventListener('change', enableSubmitButton)
+        totalPassenger.addEventListener('input', enableSubmitButton)
+
+        function enableSubmitButton() {
+            if (
+                typeDriver.value !== 'Pilih Tipe Driver' &&
+                date.value !== '' &&
+                pickupTime.value !== 'Pilih Waktu' &&
+                totalPassenger.value !== ''
+            ) {
+                submitButton.removeAttribute('disabled')
+            } else {
+                submitButton.setAttribute('disabled', true)
+            }
+        }
+    }
 }
 
-const app = new App();
-app.init();
+const app = new App()
+app.init()
