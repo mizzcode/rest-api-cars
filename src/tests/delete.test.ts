8736414f7ea8a593d61fd9b4c5eeda7daa3f7b3b
Misbahudin <misbahudin.id@gmail.com>
2024-01-05 22:56:54 +0700

    test: add create car

--- src/tests/delete.test.ts
+++ src/tests/delete.test.ts
@@ -3,12 +3,14 @@ import { describe, it, expect } from 'vitest'
 import supertest from 'supertest'
 import App from '../app'
 import { login } from '../utils/login'
+import path from 'path'
+import jsonCar from './car.json'
 
 describe('test cars module', () => {
     const app: Express = new App().app
 
     let token: string = ''
-    const id: string = ''
+    let id: string = ''
 
     it('should be able to login superadmin', async () => {
         const response = await login(supertest, app, 'mizz@gmail.com', 'password')
@@ -16,6 +18,39 @@ describe('test cars module', () => {
         expect(response).toBeTruthy()
 
         token = response.body.token
+    }, 70000)
+
+    it('should be create a new car', async () => {
+        const st = supertest(app)
+            .post('/api/v1/cars')
+            .attach('image', path.resolve(__dirname, 'car.jpg'))
+            .set({
+                'Content-Type': 'multipart/form-data',
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json',
+            })
+
+        // @ts-expect-error delete req column
+        delete jsonCar.id
+        // @ts-expect-error delete req column
+        delete jsonCar.image
+        // @ts-expect-error delete req column
+        delete jsonCar.availableAt
+
+        for (const key in jsonCar) {
+            // @ts-expect-error any
+            const value = jsonCar[key]
+            if (key !== 'image') {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+                void st.field(key, value)
+            }
+        }
+
+        const response = await st
+
+        expect(response.statusCode).toBe(201)
+
+        id = response.body.data.carId
     }, 70000)
 
     it('should be delete all car', async () => {
